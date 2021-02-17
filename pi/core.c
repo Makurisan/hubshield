010364425dbded551930d95a65a779315433d5ad
Makurisan <ManfredKubica@web.de>
2021-02-17 15:19:56 +0000

    dev changes

--- pi/core.c
+++ pi/core.c
@@ -189,20 +189,29 @@ static u16 variant = 0;
 			pr_hex(vhub->transfer, 16);
 		}
 #else
-		if (variant == 0) {
-			int ret = spi_buf_rd(vhub, MASTER_RX_CMD, vhub->transfer, 4);
-			if (vhub->transfer[2] != 0 && vhub->transfer[3] != 0)
-				variant++;
-			UDCDBG(vhub, "->");
-			pr_hex(vhub->transfer, 16);
-		} else {
-			u8 idx;
-			for (idx = 0; idx < 5; ++idx)
-			{
-				_spi_buf_rd(vhub, MASTER_RX_CMD, vhub->transfer, 4);
-				pr_hex(vhub->transfer, 16);
-			}
-		}
+		// if (variant == 0) {
+		// 	int ret = spi_buf_rd(vhub, MASTER_RX_CMD, vhub->transfer, 4);
+		// 	if (vhub->transfer[2] != 0 && vhub->transfer[3] != 0)
+		// 		variant++;
+		// 	UDCDBG(vhub, "->");
+		// 	pr_hex(vhub->transfer, 16);
+		// } else {
+		// 	u8 idx;
+		// 	for (idx = 0; idx < 5; ++idx)
+		// 	{
+		// 		_spi_buf_rd(vhub, MASTER_RX_CMD, vhub->transfer, 4);
+		// 		pr_hex(vhub->transfer, 16);
+		// 	}
+		// }
+		UDCDBG(vhub, "Header");
+		_spi_buf_rd(vhub, MASTER_RX_CMD, vhub->transfer, 3);
+		pr_hex(vhub->transfer, 3);		
+		// for (idx = 0; idx < 5; ++idx)
+		// {
+		// 	_spi_buf_rd(vhub, MASTER_RX_CMD, vhub->transfer, 4);
+		// 	pr_hex(vhub->transfer, 16);
+		// }
+
 
 #endif
 		mutex_unlock(&vhub->spi_bus_mutex);
