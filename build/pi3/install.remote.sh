9bfa63a9916d1d4d13a614264ab7deb4521c8491
Manfred Kubica <manfredkubica@web.de>
2021-02-20 13:18:58 +0100

    Pi4 included

--- build/pi3/install.remote.sh
+++ build/pi3/install.remote.sh
@@ -2,7 +2,7 @@
 set -e
 git rev-parse --show-toplevel
 # ssh pi@pi3 "cd ~/vusb-build; source ./notify.sh"
-ssh pi@pi3 "clear"
+#ssh pi@pi3 "clear"
 # kernel
 scp ./build/v-hub.ko pi@pi3:/home/pi/vusb-build
 # device tree
