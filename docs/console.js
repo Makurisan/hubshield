013705995b57f6a73e96731f67bcf97098cc4d29
Manfred Kubica <ManfredKubica@web.de>
2021-08-19 18:52:07 +0200

    printf corrected

--- docs/console.js
+++ docs/console.js
@@ -51,6 +51,7 @@ function setupHterm() {
         io.onVTKeystroke = str => {
             switch (str) {
                 case '\r':
+                    input.unshift([':']);
                     input.unshift([input.length]);
                     printHub(input.join(""));
                     io.println('');
