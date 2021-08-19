9688c9463a75116c1f24d868dc0e9c5cd55d6132
Manfred Kubica <ManfredKubica@web.de>
2021-08-19 14:07:43 +0200

    Dom..

--- docs/console.js
+++ docs/console.js
@@ -15,6 +15,7 @@ lib.registerInit('load messages', async () => {
 });
 
 function setupHterm() {
+
     'use strict';
     var port;
     let textEncoder = new TextEncoder();
@@ -122,91 +123,5 @@ function setupHterm() {
             }
         });
     });
-
-}
-
-function _setupHterm() {
-  'use strict';
-
-  var port;
-
-  let textEncoder = new TextEncoder();
-
-  hterm.defaultStorage = new lib.Storage.Local();
-  let term = new hterm.Terminal;
-
-  term.onTerminalReady = () => {
-    console.log('Terminal ready.');
-    let io = term.io.push();
-
-    io.onVTKeystroke = str => {
-      if (port !== undefined) {
-        port.send(textEncoder.encode(str)).catch(error => {
-          term.io.println('Send error: ' + error);
-        });
-      }
-    };
-
-    io.sendString = str => {
-      if (port !== undefined) {
-        port.send(textEncoder.encode(str)).catch(error => {
-          term.io.println('Send error: ' + error);
-        });
-      }
-    };
-
-      term.decorate(document.querySelector('#terminal'));
-      term.setWidth(80);
-      term.setHeight(50);
-      term.installKeyboard();
-  };
-
-  document.addEventListener('DOMContentLoaded', event => {
-    let connectButton = document.querySelector('#connect');
-
-      
-
-    function connect() {
-      term.io.println('Connecting to ' + port.device_.productName + '...');
-      port.connect().then(() => {
-        console.log(port);
-        term.io.println('Connected.');
-        connectButton.textContent = 'Disconnect';
-        port.onReceive = data => {
-          let textDecoder = new TextDecoder();
-          term.io.print(textDecoder.decode(data));
-        }
-        port.onReceiveError = error => {
-          term.io.println('Receive error: ' + error);
-        };
-      }, error => {
-        term.io.println('Connection error: ' + error);
-      });
-    };
-
-    connectButton.addEventListener('click', function() {
-      if (port) {
-        port.disconnect();
-        connectButton.textContent = 'Connect';
-        port = null;
-      } else {
-        serial.requestPort().then(selectedPort => {
-          port = selectedPort;
-         // connect();
-        }).catch(error => {
-          term.io.println('Connection error: ' + error);
-        });
-      }
-    });
-
-    serial.getPorts().then(ports => {
-      if (ports.length == 0) {
-        term.io.println('No devices found.');
-      } else {
-        port = ports[0];
-       // connect();
-      }
-    });
-  });
 
 }
